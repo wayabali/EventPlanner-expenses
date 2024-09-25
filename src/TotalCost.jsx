22f2a676c15b782353df1fac13040441597d8363
wayabali <ryadbali60@.com>
2024-09-25 12:22:21 -0400

    last

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
-const TotalCost = ({ totalCosts, ItemsDisplay }) => {
+const TotalCost = ({ totalCosts, ItemsDisplay , handleClick }) => {
   
+    const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
   return (
     <div className="pricing-app">
@@ -12,12 +13,12 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
+           ${total_amount}
           </h2>
-         
-            <div>
-             
+            <div className='render_items'>
+               {ItemsDisplay && <ItemsDisplay />}
             </div>
+            <button className='details_button' style={{marginTop : 15}} onClick={handleClick}>RETURN</button>
         </div>
       </div>
     </div>
